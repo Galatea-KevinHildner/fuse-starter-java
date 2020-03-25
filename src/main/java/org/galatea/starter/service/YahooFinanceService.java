dd9a9b958370850a043c6e8acf85ae3c883055e5
Kevin Hildner <Kevin.Hildner@galatea-associates.com>
2020-03-25 13:33:23 -0400

    Created JavaUrlConnectionReader and integrated it into YahooFinanceService

--- src/main/java/org/galatea/starter/service/YahooFinanceService.java
+++ src/main/java/org/galatea/starter/service/YahooFinanceService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.LinkedList;
 import org.galatea.starter.utils.YahooFinanceData.PriceInfo;
 import org.galatea.starter.utils.YahooFinanceData.Request;
+import org.galatea.starter.utils.JavaUrlConnectionReader;
 
 public class YahooFinanceService {
+  private final String alphaVantageKey = "KBOP9W9HW83OI5DP";
+  private JavaUrlConnectionReader urlConnectionReader;
+
 
   /**
    * Attempts to collect the data specified in the Request object
@@ -34,8 +38,25 @@ public class YahooFinanceService {
   /**
    * Attempts to retrieve price information from alphavantage
    */
-  private PriceInfo getFromWeb(String Ticker, Date date, int id) {
-    //TODO: Implement
-    return NULL;
+  private boolean getFromWeb(String ticker) {
+    // Build the URL
+    StringBuilder url = new StringBuilder();
+    url.append("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=");
+    url.append(ticker);
+    url.append("&apikey=");
+    url.append(alphaVantageKey);
+
+    // Get the data from alphavantage
+    String rawData = urlConnectionReader.getUrlContents(url);
+    String avErrMsg = "{\n"
+        + "    \"Error Message\": \"Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for TIME_SERIES_DAILY.\"\n"
+        + "}\n";
+
+    if(rawData.equals(avErrMsg)){
+      return false;
+    }
+
+    // Add data to database
+    //TODO: Extract data and add it to MySQL server
   }
 }
